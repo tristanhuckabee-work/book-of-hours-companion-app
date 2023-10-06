6a1877ddd2c1f622b2a95041dec745db0bf86358
tristan <thuckabee.work@gmail.com>
2023-10-06 00:15:28 -0500

    added validators to login and signup

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
 
-router.post('/', async (req, res, next) => {
-  const { credential, password } = req.body;
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 
+router.post('/', validateLogin, async (req, res, next) => {
+  const { credential, password } = req.body;
   const user = await User.unscoped().findOne({
-    where: {
-      [Op.or]: {
+    where: { [Op.or]: {
         username: credential,
         email: credential
       }
